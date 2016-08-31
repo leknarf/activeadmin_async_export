1743b52f39870f49f28c906235798668b6c23e8c
Justin Louie <jslouie@gmail.com>
2016-08-31 16:25:26 +0800

    Add send filtering params to background job

--- lib/active_admin/async_export/resource_controller_extension.rb
+++ lib/active_admin/async_export/resource_controller_extension.rb
@@ -12,7 +12,7 @@ module ActiveAdmin
           format.email do
             current_user_method = active_admin_config.namespace.application.current_user_method
             admin_email = send(current_user_method).email
-            ActiveAdmin::AsyncExport::AsyncExportMailer.delay.csv_export(admin_email, collection.first.class.to_s)
+            ActiveAdmin::AsyncExport::AsyncExportMailer.delay.csv_export(admin_email, collection.first.class.to_s, params.to_json)
             redirect_to :back, notice: "CSV export emailed to #{admin_email}!"
           end
         end
